3c34c3c70f01046925be1a6705acd61643a1216e
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-15 08:32:48 -0400

    added image bg and greidient background

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div>
-      
-    </div>
+      <div
+        className="w-full min-h-[500px] flex items-center justify-center"
+        style={{
+          backgroundImage: "url('/header.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+      <div className="flex flex-col items-center justify-center text-center p-8 bg-gradient-to-r from-blue-400 to-purple-400 text-white h-screen">
+        Hello
+      </div>
     </>
   );
 }
