444cf73c611212399a35997400a70e70d15d06a0
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-16 08:49:32 -0400

    item area converter

---
+++ app/ItemArea/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import convertToSubcurrency from "@/lib/converSubcurrency";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutPage from "../components/CheckoutPage";
+
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+  throw new Error("Missing Stripe public key in environment variables");
+}
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || ""
+);
+
+export default function ItemArea() {
+  const amount = 5.0;
+  return (
+    <>
+    <div className="min-h-screen bg-gradient-to-r from-blue-100 to-purple-100 flex flex-col items-center py-12">
+      Final Payment ${amount}
+    </div>
+    <Elements stripe={stripePromise}
+     options={{
+      mode: "payment",
+      amount: convertToSubcurrency(amount),
+      currency: "usd",
+     }}
+    >
+        <CheckoutPage amount={amount} />
+      </Elements>
+      </>
+  );
+}
