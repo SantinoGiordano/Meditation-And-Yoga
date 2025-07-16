258cf17c5db9c79740bb61d0efc09439daafad5c
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-16 09:08:22 -0400

    added payment method

--- app/components/CheckoutPage.tsx
+++ app/components/CheckoutPage.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import convertToSubcurrency from "@/lib/converSubcurrency";
 import {
   useStripe,
   useElements,
   PaymentElement,
 } from "@stripe/react-stripe-js";
 import { Check } from "lucide-react";
-import { use, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 const CheckoutPage = ({ amount }: { amount: number }) => {
   const stripe = useStripe();
@@ -15,6 +16,30 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
   const [errorMessage, setErrorMessage] = useState<string>();
   const [clientSecret, setClientSecret] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetch("/api/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(convertToSubcurrency(amount)),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [amount]);
+
+  return (
+    <>
+    <form>
+        {clientSecret && <PaymentElement/>}
+        <button>
+            Pay 
+        </button>
+    </form>
+    </>
+  )
 };
+
 
 export default CheckoutPage;
