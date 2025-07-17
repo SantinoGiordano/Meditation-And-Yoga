c1d1297cedc7064af28efbc88f2618e6c6f5d9db
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-16 21:07:21 -0400

    changed form of the stripe import

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY!);
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: "2025-06-30.basil" });
 
 export async function POST(request: NextRequest) {
   try {
