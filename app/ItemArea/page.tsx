ca99b70b631fb851965acbde52ef931e436ed4b7
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-16 21:49:17 -0400

    increased warning size

--- app/ItemArea/page.tsx
+++ app/ItemArea/page.tsx
@@ -18,7 +18,7 @@ export default function ItemArea() {
   return (
     <>
       <main className="min-h-screen bg-gradient-to-r from-blue-100 to-purple-100 flex flex-col items-center py-12">
-        <div className="flex-1 bg-white bg-opacity-50 rounded-lg p-6 shadow-md border-l-4 border-red-500 max-h-50 max-w-150 m-10">
+        <div className="flex-1 bg-white bg-opacity-50 rounded-lg p-6 shadow-md border-l-4 border-red-500 max-h-50 max-w-350 m-10">
           <h3 className="text-2xl font-bold mb-4 text-red-500">Hello, Please Read</h3>
           <p className=" mb-4 text-sm leading-relaxed text-gray-700">
             if you have come to this part of the website just know that everything is in an unstable not finsihed state, nothing here is perminate nothing here works yet, everything is being worked on. the entire website is a work in progress, eveything site wide is subject ot change
